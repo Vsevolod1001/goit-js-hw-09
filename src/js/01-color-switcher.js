eb0711ea2b2c4769522e36cc8d405ac8c695a119
Vsevolod1001 <sevant.101@gmail.com>
2021-11-07 22:06:56 +0200

    дз1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
+
+
+startBtn.addEventListener('click', startColor);
+stopBtn.addEventListener('click', stopColor);
+
+let timerId;
+
+function startColor(e) {
+    timerId = setInterval(changeColor, 1000); 
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopColor (e) {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+
+function changeColor() {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+}
+
+export function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
